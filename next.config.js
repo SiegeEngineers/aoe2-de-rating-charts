882be0549824d7410e42ffa634572a1b29fa6250
thbrown <thomasaustenbrown@gmail.com>
2020-03-22 21:52:01 -0600

    Another attempt to get github pages working

--- next.config.js
+++ next.config.js
@@ -1,12 +1,22 @@
-const isProd = (process.env.NODE_ENV || 'production') === 'production'
-//process.env.DEPLOY_ENV === 'GH_PAGES'
+const debug = process.env.NODE_ENV !== "production";
 
 module.exports = {
-    exportTrailingSlash: true,
-    exportPathMap: function() {
-        return {
-            '/': { page: '/' }
-        };
-    },
-    assetPrefix: isProd ? '/aoe2-de-elo-histogram' : '',
-};
+  exportPathMap: function () {
+    return {
+      "/": { page: "/" },
+    }
+  },
+  assetPrefix: !debug ? '/aoe2-de-elo-histogram/' : '',
+  webpack: (config, { dev }) => {
+    // Perform customizations to webpack config
+    console.log('webpack');
+    config.module.rules = config.module.rules.map(rule => {
+      if(rule.loader === 'babel-loader') {
+        rule.options.cacheDirectory = false
+      }
+      return rule
+    })
+    // Important: return the modified config
+    return config
+  }
+}
