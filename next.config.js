1d194379f327e465538a1f2ee9a94ab31a843ada
thbrown <thomasaustenbrown@gmail.com>
2020-03-22 21:23:36 -0600

    Add assetPrefix to hopefully get this workign with githubPages

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
+const isProd = (process.env.NODE_ENV || 'production') === 'production'
+//process.env.DEPLOY_ENV === 'GH_PAGES'
+
 module.exports = {
     exportTrailingSlash: true,
     exportPathMap: function() {
         return {
             '/': { page: '/' }
         };
-    }
+    },
+    assetPrefix: isProd ? '/aoe2-de-elo-histogram' : '',
 };
