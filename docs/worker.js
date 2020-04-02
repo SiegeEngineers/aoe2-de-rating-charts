64477a70def872a6cee7d39e52b3236b6c7dcd9b
thbrown <thomasaustenbrown@gmail.com>
2020-04-02 20:03:32 +0000

    Update page

--- docs/worker.js
+++ docs/worker.js
@@ -1,32 +1,35 @@
 // worker.js
 self.addEventListener(
   "message",
-  function(e) {
-    let data = e.data;
+  function(message) {
+    let data = message.data;
     let needle = data.query;
     let haystack = data.data;
     let maxCandidates = data.limit;
+    let blacklist = data.blacklist ? data.blacklist : [];
     let t0 = performance.now();
+
+    if (needle == null || needle == undefined) {
+      self.postMessage([]);
+      return;
+    }
 
     let candidates = [];
     needle = simplifyString(needle);
     for (let i = 0; i < haystack.length; i++) {
-      let candidateString = simplifyString(haystack[i][0]);
-      if (candidateString.includes(needle)) {
-        candidates.push({
-          value: haystack[i],
-          label: haystack[i][0]
-        });
+      let candidateString = simplifyString(haystack[i].label);
+      if (
+        candidateString.includes(needle) &&
+        !blacklist.includes(haystack[i].value)
+      ) {
+        candidates.push(haystack[i]);
       }
       if (candidates.length >= maxCandidates) {
         break;
       }
     }
     var t1 = performance.now();
-    console.log(
-      "Webworker query took " + (t1 - t0) + " milliseconds.",
-      haystack.length
-    );
+    console.log("Webworker query took " + (t1 - t0) + " milliseconds.");
     self.postMessage(candidates);
   },
   false
