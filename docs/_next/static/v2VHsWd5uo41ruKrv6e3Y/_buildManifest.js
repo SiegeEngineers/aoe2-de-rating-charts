40ce7cbb94cd8d67ce135b4d20cdcd79539e9a1d
thbrown <thomasaustenbrown@gmail.com>
2020-11-01 00:07:20 +0000

    Update page

--- docs/_next/static/v2VHsWd5uo41ruKrv6e3Y/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = (function(a,b){return {"/":[a,b],"/deathmatch":[a,b],"/index":[a,b]}}("static\u002Fchunks\u002F0b48f80119d8d84a3153238fb0358150777f3b67.9f3c1a46911b23c157f6.js","static\u002Fcss\u002F6bb9739068a8e3900c17.css"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
