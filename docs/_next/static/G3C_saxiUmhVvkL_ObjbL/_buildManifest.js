0126848d958bb603eac9791df797bc65ea00a3dc
thbrown <thomasaustenbrown@gmail.com>
2020-12-11 00:08:29 +0000

    Update page

---
+++ docs/_next/static/G3C_saxiUmhVvkL_ObjbL/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a,b){return {"/":[a,b],"/deathmatch":[a,b],"/index":[a,b]}}("static\u002Fchunks\u002F3731890bf1ff3f3fe5f753bb0bb8829ca45f9fa3.ffd7b1bb534c1f0b9076.js","static\u002Fcss\u002F6bb9739068a8e3900c17.css"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
--- docs/_next/static/qN1FlN1nPvDO0t6TdWerl/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = (function(a,b){return {"/":[a,b],"/deathmatch":[a,b],"/index":[a,b]}}("static\u002Fchunks\u002F52d3ea38fa77bf2d64a4beea5c4dc0526661ec10.ffd7b1bb534c1f0b9076.js","static\u002Fcss\u002F6bb9739068a8e3900c17.css"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
