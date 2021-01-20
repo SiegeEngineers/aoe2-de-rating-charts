a9417b0f182a4db981578a5e551d116f76ee4000
thbrown <thomasaustenbrown@gmail.com>
2021-01-20 00:41:08 +0000

    Update page

--- docs/_next/static/xd1bZhYjcITneAF08aaN8/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = (function(a,b){return {"/":[a,b],"/deathmatch":[a,b],"/index":[a,b]}}("static\u002Fchunks\u002Fdb42272a21e3d96b329e0951a1f3648a049b9c0d.9f3c1a46911b23c157f6.js","static\u002Fcss\u002F6bb9739068a8e3900c17.css"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
